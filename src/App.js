f3f66d8d84f2a38ee3837b60b7dba6cce28ece5e
Atharva Shinde <atharva09shinde@gmail.com>
2025-08-02 13:13:32 +0530

    in feature branch 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { learnRedux } from './components';
 import ButtonDisplay from './components/ButtonDisplay';
 
 function App() {
   return (
     <>
     <ButtonDisplay />
+    <learnRedux />
     </>
   );
 }
---
+++ src/components/learnRedux.jsx
@@ -0,0 +1,10 @@
+
+export const learnRedux = () => {
+
+    return(
+        <>
+            <h1>Learn Redux</h1>
+            <p>Redux is a predictable state container for JavaScript apps.</p>  
+        </>
+    );
+}
